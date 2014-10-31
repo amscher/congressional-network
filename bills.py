0c4d4836dc057a141113063d1f03a67af9a9a1b8
Montana Scher <amscher@gmail.com>
2014-10-31 15:40:17 -0700

    updated bills graph

--- bills.py
+++ bills.py
@@ -2,17 +2,18 @@ import json
 import snap
 
 bill_sponsors_map = {}
+sponsor_bills_map = {}
 
-def addBillEdgesToNetwork(G, sponsors):
+def addBillEdgesToNetwork(graph, sponsors):
   for i in range(0, len(sponsors)):
     for j in range(i+1, len(sponsors)):
-      G.AddEdge(int(sponsors[i]), int(sponsors[j]))
+      graph.AddEdge(int(sponsors[i]), int(sponsors[j]))
 
 def createNetwork(Nodes):
-  G = snap.TUNGraph.New()
+  graph = snap.TUNGraph.New()
   for NId in Nodes:
-    G.AddNode(NId)
-  return G
+    graph.AddNode(NId)
+  return graph
 
 def getBills(bill_type, bill_type_id):
   for i in range(1000):
@@ -37,7 +38,6 @@ def getBills(bill_type, bill_type_id):
     bill_id = "%s%s" % (bill_type_id, bill_id)
     bill_sponsors_map[int(bill_id)] = cosponsors
 
-sponsor_bills_map = {}
 
 def createSponsorToBillsMap():
   for key in bill_sponsors_map:
@@ -50,13 +50,24 @@ def createSponsorToBillsMap():
         sponsor_bills_map[sponsor] = [key]
 
 getBills("hr", 0)
+getBills("hjres", 1)
 createSponsorToBillsMap()
+for key in sponsor_bills_map:
+  print "%s : " % (key),
+  bills = sponsor_bills_map[key]
+  for i in range(len(bills)):
+    print "%d, " % (bills[i]),
+  print "\n"
 
-G = createNetwork(bill_sponsors_map.keys()) # creates network where a node is a bill
+graph = createNetwork(bill_sponsors_map.keys()) # creates network where a node is a bill
 for thomas_id in sponsor_bills_map:
-    addBillEdgesToNetwork(G, sponsor_bills_map[thomas_id])
+    addBillEdgesToNetwork(graph, sponsor_bills_map[thomas_id])
 
-print "Graph has %d nodes and %d edges" % (G.GetNodes(), G.GetEdges())
-print "Its clustering co-efficient is %f" % (snap.GetClustCf(G))
-
+print "Graph has %d nodes and %d edges" % (graph.GetNodes(), graph.GetEdges())
+print "Its clustering co-efficient is %f" % (snap.GetClustCf(graph))
+print "Its effective diameter is %d" % (snap.GetBfsEffDiam(graph, 20, False))
+DegToCntV = snap.TIntPrV()
+snap.GetDegCnt(graph, DegToCntV)
+for item in DegToCntV:
+    print "%d nodes with degree %d" % (item.GetVal2(), item.GetVal1())
 
