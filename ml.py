0aa2717de8554243896def7d379b99368ded43c4
neeral <neeral.dodhia@gmail.com>
2014-12-03 15:04:04 -0800

    - Added half-baked forward search algorithm. It needs manual intervention to set up for the next round of iterations when you add a new feature to the set F of chosen features. - Changed to using Stochastic Gradient Ascent as mathematical description of l2 LogisticRegression in sklearn was similar to SVM. - Added following statistics for the predictions: precision, recall and F1 score.

--- ml.py
+++ ml.py
@@ -11,7 +11,9 @@ http://scikit-learn.org/stable/modules/svm.html
 http://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html#sklearn.svm.SVC
 """
 import numpy
+import scipy
 import sklearn.linear_model
+from sklearn.metrics import classification_report
 import csv
 
 import Bill
@@ -83,32 +85,32 @@ def runML(prepareDatasetFunction):
 def runML((features, labels)):
     X_train = numpy.array(features[:int(len(features)*.7)])
     y_train = numpy.array(labels[:int(len(labels)*.7)])
-    print '%d training examples' % len(X_train)
-
     X_test = numpy.array(features[int(len(features)*.7):])
     y_test = numpy.array(labels[int(len(labels)*.7):]) 
-    print '%d test samples' % len(X_test)
+    print '%d training examples, %d test samples, %d in total' % (len(X_train), len(X_test), len(labels))
 
-    score = doLogisticRegression(X_train, y_train, X_test, y_test)
-    print '\nLogistic Regression score: {0:.2f}%'.format(100*score)
-    score = doSVM(X_train, y_train, X_test, y_test)
+    (score, classification_report) = doLogisticRegression(X_train, y_train, X_test, y_test)
+    print '\nLogistic Regression (SGD) score: {0:.2f}%'.format(100*score)
+    print classification_report
+    (score, classification_report) = doSVM(X_train, y_train, X_test, y_test)
     print 'SVM score: {0:.2f}%'.format(100*score)
+    print classification_report
 
 
 def doLogisticRegression(X_train, y_train, X_test, y_test):
-    # initialise model with some random parameters
-    # extension would be to experiment with these
-    clf_l2_LR = sklearn.linear_model.LogisticRegression(C=100, penalty='l2', tol=0.01)
-    clf_l2_LR.fit(X_train, y_train)
-    return clf_l2_LR.score(X_test, y_test)
+    # changed to using Stochastic Gradient Ascent as l2 LogisticRegression in sklearn was similar to SVM
+    clf = sklearn.linear_model.SGDClassifier().fit(X_train, y_train)
+    predictions = clf.predict(X_test)
+    return (clf.score(X_test, y_test), classification_report(y_test, predictions))
 
 def doSVM(X_train, y_train, X_test, y_test):
     clf = sklearn.svm.SVC()
-    clf.fit(X_train, y_train)
-    return clf.score(X_test, y_test)
+    clf.fit(scipy.sparse.csr_matrix(X_train), y_train)
+    sparse_X_test = scipy.sparse.csr_matrix(X_test)
+    predictions = clf.predict(sparse_X_test)
+    # doesn't matter whether you use a sparse or dense matrix in score()
+    return (clf.score(X_test, y_test), classification_report(y_test, predictions))
 
-# write data to a CSV file
-# three columns: bill_id | # cosponsors | month it was introduced
 def read_csv(file_path, bill_predicate):
     features = []
     labels = []
@@ -130,7 +132,6 @@ def read_csv(file_path, bill_predicate):
                     label = 1 if status in Bill.SUCCESSFUL else 0
             features.append(feature)
             labels.append(label)
-    print len(labels)
     return (features, labels)
 
 if __name__ == '__main__':
@@ -138,10 +139,10 @@ if __name__ == '__main__':
     #(features, labels) = read_csv('./bill_combined_All.csv', 'OutOfCommittee')
     # to find all CSV files: find . -maxdepth 2 -name '*.csv'
 
+    print '*** Predicting whether a bill will get out of committee ***'
     #runML(prepareInputDataForGettingOutOfCommittee)
-    print 'Predicting whether a bill will get out of committee'
     runML(read_csv('./bill_combined.csv', 'OutOfCommittee'))
-    print '\n\nPredicting whether a bill will get enacted given it is out of committee'
+    print '\n\n*** Predicting whether a bill will get enacted given it is out of committee ***'
     #runML(prepareInputDataAfterCommitteeGettingEnacted)
     runML(read_csv('./bill_combined.csv', 'Enacted'))
 
